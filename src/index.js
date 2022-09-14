66c83a988ed06eea27789c31544a5879611bb752
TayCarlson <taymarie3@outlook.com>
2022-09-14 14:23:07 -0700

    removed console.log

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thur", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -45,7 +44,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "922a961e9118b841298079a3b004b27d";
   let apiUrl = `http://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
